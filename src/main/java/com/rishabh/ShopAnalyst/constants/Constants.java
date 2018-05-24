5b9bd168c23235a2ab1cae2c91da02fcf26da8a5
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-05-24 10:01:43 -0500

    Add constants file

---
+++ src/main/java/com/rishabh/ShopAnalyst/constants/Constants.java
@@ -0,0 +1,38 @@
+package com.rishabh.ShopAnalyst.constants;
+
+public class Constants {
+
+    public final String DB_NAME = "ShopAnalystsDB";
+    public final String COLL_NAME = "ShopAnalyst";
+
+    public final static String JSON_FILE_LOCATION = "";
+
+    public final static String  ID = "pnum";
+    public final static String  DOB = "dob";
+    public final static String  CAPTION = "caption";
+    public final static String  ETHNICITY = "ethnicity";
+    public final static String  WEIGHT = "weight";
+    public final static String  HEIGHT = "height";
+    public final static String  isVEG = "is_veg";
+    public final static String  isDRINK = "drink";
+
+    public final static int GRAMS_TO_KGS = 1000;
+
+    public final static int PAGE_SIZE = 10;
+
+    public static int getEthinicityCode(final String ethn) {
+        switch(ethn){
+            case "Asian" : return 0;
+            case "Indian" : return 1;
+            case "African Americans" : return 2;
+            case "Asian Americans" : return 3;
+            case "European" : return 4;
+            case "British" : return 5;
+            case "Jewish" : return 6;
+            case "Latino" : return 7;
+            case "Native American" : return 8;
+            case "Arabic" : return 9;
+        }
+        return -1;
+    }
+}
