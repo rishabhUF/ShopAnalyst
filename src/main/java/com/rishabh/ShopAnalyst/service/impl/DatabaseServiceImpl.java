1bef450d42e3ad0786ab7fcbc9ff695edcd63512
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-05-25 12:45:40 -0500

    Add Database Service Implementation

--- src/main/java/com/rishabh/ShopAnalyst/service/impl/DatabaseServiceImpl.java
+++ src/main/java/com/rishabh/ShopAnalyst/service/impl/DatabaseServiceImpl.java
@@ -1,23 +1,9 @@
 package com.rishabh.ShopAnalyst.service.impl;
 
-import com.google.gson.Gson;
-import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
-import com.rishabh.ShopAnalyst.constants.Constants;
 import com.rishabh.ShopAnalyst.doa.CustomeMongoDao;
-import com.rishabh.ShopAnalyst.domains.Member;
 import com.rishabh.ShopAnalyst.service.DatabaseService;
-import com.rishabh.ShopAnalyst.util.Jsonreader;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Service
 public class DatabaseServiceImpl implements DatabaseService {
@@ -36,49 +22,21 @@ public class DatabaseServiceImpl implements DatabaseService {
 
     @Override
     public String insertData() {
-        MongoClient client = null;
-        try {
-            client = new MongoClient("localHost", 27017);
-            MongoDatabase db = client.getDatabase(Constants.DB_NAME);
 
-            // First always drop the collections.
-            MongoCollection<Document> collection = db.getCollection(Constants.COLL_NAME);
-            collection.drop();
-
-            // Now insert the data from Json
-            Jsonreader jsonreader = new Jsonreader(collection);
-            int count = jsonreader.loadDataInMongo();
-            return "Inserted " + count + "records in mongo db" + "\nDB_NAME = " + Constants.DB_NAME + "\nCOLLECTION = " + Constants.COLL_NAME;
-        } finally {
-            if (client != null) {
-                client.close();
-            }
-        }
+        String result = "";
+        result = mongoDao.insertData();
+        if(result.isEmpty())
+            return "Data is not inserted property";
+        return result;
     }
 
     @Override
     public String displayData() {
 
-            MongoClient client = null;
-            List<Member> memberData = new ArrayList<Member>();
-            try {
-                client = new MongoClient("localhost", 27017);
-                MongoDatabase db = client.getDatabase(Constants.DB_NAME);
-                FindIterable<Document> cursorIterator = db.getCollection(Constants.COLL_NAME).find();
-                for (Document cursor : cursorIterator) {
-                    final Member member = Member.getMemberFromMongo(cursor);
-                    memberData.add(member);
-                }
-            }finally {
-                if (client != null)
-                    client.close();
-            }
-        if (memberData.size() == 0) {
-            final Member member = new Member();
-            member.setErrorMessage("No element found. Empty Collection");
-            memberData.add(member);
-        }
-        Gson gson = new Gson();
-        return gson.toJson(memberData);
+        String result = "";
+        result = mongoDao.displayData();
+        if(result.isEmpty())
+            return "There is no data store in the DB";
+        return result;
     }
 }
